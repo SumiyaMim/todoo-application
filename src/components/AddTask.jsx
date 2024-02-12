e8b63dc4171a3c984e3895af4b7bf4bdc147aa20
SumiyaMim <sumiyaislam1093@gmail.com>
2024-02-12 20:46:08 +0600

    add task

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const AddTask = () => {
+  return (
+    <div className='py-20 px-8 md:px-10 lg:px-14 max-w-md md:max-w-3xl lg:max-w-7xl mx-auto'>
+      <h1 className='text-center font-bold text-2xl mb-6'>Add Task</h1>
+      <div className='border p-8 md:p-12 rounded-md border-zinc-300'>
+        <input type="text" placeholder="New Task" className="input input-bordered rounded-md w-full outline-none focus:outline-none mb-5" />
+        <div className='flex flex-col md:flex-row md:items-center gap-7 mb-7'>
+          <h2 className='text-zinc-400 font-medium'>Priority Level:</h2>
+            <div className='flex items-center gap-2'>
+                <input type="radio" name="priority" className="radio checked:bg-[#4568da]" />
+                <span className="font-medium">High</span> 
+            </div>
+            <div className='flex items-center gap-2'>
+                <input type="radio" name="priority" className="radio checked:bg-[#4568da]" />
+                <span className="font-medium">Medium</span> 
+            </div>
+            <div className='flex items-center gap-2'>
+                <input type="radio" name="priority" className="radio checked:bg-[#4568da]" />
+                <span className="font-medium">Low</span> 
+            </div>
+        </div>
+        <button className='w-full bg-[#4568da] text-white p-1.5 rounded-md font-medium'>Add New Task</button>
+      </div>
+    </div>
+  )
+}
+
+export default AddTask
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import Header from '../components/Header'
+import AddTask from '../components/AddTask'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <AddTask/>
     </div>
   )
 }
